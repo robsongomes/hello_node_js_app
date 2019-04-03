04ee6e8bcee64ba01bab3cad94e259f71f310403
Robson Gomes <nosbor84@gmail.com>
2019-04-03 20:57:45 -0300

    changing hello message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let http = require('http')
 
 let server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"})
-  res.end("Hello World")
+  res.end("Welcome to my node site")
 })
 
 var port = process.env.PORT || 3000;
